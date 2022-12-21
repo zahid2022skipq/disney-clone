db619484238afcfe14a4a05e4c3c5d553d3e9ba3
zahid2022slipq <zahid.ali.skipq@gmail.com>
2022-12-21 14:26:01 +0500

    adding styled components

--- src/components/Login.js
+++ src/components/Login.js
@@ -14,8 +14,21 @@ const Container = styled.section`
   display: flex;
   flex-direction: column;
   text-align: center;
+  height: 100vh;
 `;
 
-const Content = styled.div``;
+const Content = styled.div`
+  margin-bottom: 10vw;
+  width: 100%;
+  position: relative;
+  min-height: 100vh;
+  box-sizing: border-box;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  padding: 80px 40px;
+  height: 100%;
+`;
 
 export default Login;
