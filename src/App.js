3ce40c7546f34ae7c65a98a2764adf4c09e2abf7
zahid2022slipq <zahid.ali.skipq@gmail.com>
2022-12-21 13:45:27 +0500

    Added Login component and a ROuter

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+
 import "./App.css";
+import Login from "./components/Login";
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello World</h1>
+      <Router>
+        <Routes>
+          <Route path="/login" element={<Login />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
