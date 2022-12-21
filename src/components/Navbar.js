4e4095eb84a1fd1d8f79b7e44f54131e7feb2745
zahid2022slipq <zahid.ali.skipq@gmail.com>
2022-12-21 17:27:30 +0500

    Hoer animation

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -81,22 +81,32 @@ const NavMenu = styled.div`
       padding: 2px 0;
       white-space: nowrap;
       position: relative;
+      
+          &:before {
+            background-color: rgb(249, 249, 249);
+            border-radius: 0 0 4px 4px;
+            bottom: -6px;
+            content: "";
+            height: 2px;
+            left: 0px;
+            right: 0px;
+            opacity: 0;
+            position: absolute;
+            transform-origin: left center;
+            transform: scaleX(0);
+            transition: all 250ms cubic-bezier(0.25, 0.46, 0.45, 0.96) 0s;
+            visibility: hidden;
+            width: auto;
+          }
+
     }
 
-    a:before {
-      background-color: rgb(249, 249, 249);
-      border-radius: 0 0 4px 4px;
-      bottom: -6px;
-      content: "";
-      height: 2px;
-      opacity: 0;
-      position: absolute;
-      transform-origin: left center;
-      transform: scaleX(0);
-      transition: all 250ms cubic-bezier(0.25, 0.46, 0.45, 0.96) 0s;
-      visibility: hidden;
-      width: auto;
-    }
+    &:hover {
+      span:before {
+        transform: scaleX(1);
+        visibility: visible;
+        opacity: 1 !important;
+      }
   }
 
   @media (max-width: 760px) {
