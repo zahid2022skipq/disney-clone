df065bd31f7e419241d9c1d4f76c96c6a93a527b
zahid2022slipq <zahid.ali.skipq@gmail.com>
2022-12-21 14:42:09 +0500

    added background image

--- src/components/Login.js
+++ src/components/Login.js
@@ -37,7 +37,13 @@ const BgImage = styled.div`
   height: 100%;
   background-position: top;
   background-size: cover;
+  background-repeat: no-repeat;
   background-image: url("images/login-background.jpg");
+  position: absolute;
+  top: 0;
+  right: 0;
+  left: 0;
+  z-index: -1;
 `;
 
 export default Login;
