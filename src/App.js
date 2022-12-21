8b8917424eae3ca45a746901c88bd0d62ea974e1
zahid2022slipq <zahid.ali.skipq@gmail.com>
2022-12-21 16:26:56 +0500

    added navbar

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 import "./App.css";
 import Login from "./components/Login";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <div className="App">
+      <Navbar />
       <Router>
         <Routes>
           <Route path="/" element={<Login />} />
