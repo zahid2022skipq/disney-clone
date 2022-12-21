da4272e9e98167f9bcb2efafcd7f8cfc854d694b
zahid2022slipq <zahid.ali.skipq@gmail.com>
2022-12-21 16:49:05 +0500

    working with navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,53 @@
 import React from "react";
+import styled from "styled-components";
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <Nav>
+      <NavImg>
+        <img src="/images/logo.svg" alt="logo" />
+      </NavImg>
+      <NavMenu>Menu</NavMenu>
+      <LoginBtn>Login</LoginBtn>
+    </Nav>
+  );
 };
+
+const Nav = styled.div`
+  position: fixed;
+  background-color: #090b13;
+  display: flex;
+  top: 0;
+  left: 0;
+  right: 0;
+  height: 70px;
+  justify-content: space-between;
+  align-items: center;
+  padding: 0 36px;
+  letter-spacing: 16px;
+  z-index: 3;
+`;
+
+const NavImg = styled.a`
+  padding: 0;
+  width: 80px;
+  margin-top: 4px;
+  max-height: 70px;
+  display: inline-block;
+  img {
+    display: block;
+    width: 100%;
+  }
+`;
+
+const LoginBtn = styled.a`
+  border: 1px solid #f9f9f9;
+  padding: 10px 10px;
+`;
+
+const NavMenu = styled.div`
+  align-items: center;
+  display: flex;
+`;
 
 export default Navbar;
